8061f92f6909e2dddce09cd33717b7fbdfec5283
ubukawa <ubukawa-t96dq@mlit.go.jp>
2022-01-16 07:49:40 -0500

    rev

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const config = require('config')
 const express = require('express')
+var router = express.Router()
+const fs = require('fs')
 const cors = require('cors') 
 const morgan = require('morgan')
 const winston = require('winston')
@@ -36,6 +38,22 @@ app.use(morgan(morganFormat, {
   stream: logger.stream
 }))
 app.use(express.static(htdocsPath))
+
+
+//Added
+const esriRouter = router.get(`/:t/VectorTileServer/`, 
+ async function(req, res) {
+  const t = req.params.t
+  var indexjsonPath = `./esri-IF/${t}/VectorTileServer/index.json`
+  if(fs.existsSync( indexjsonPath )){
+    res.sendFile('index.json', { root: `./esri-IF/${t}/VectorTileServer` })
+  }else{
+    res.status(404).send(`index.json not found: esri-IF/${t}/VectorTileServer`)
+  }
+ }
+)
+
+app.use('/esri-IF', esriRouter)
 
 //for http
 app.listen(port, () => {
